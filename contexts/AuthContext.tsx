53d03a86338f044d86afd036c567943e96f10073
SaturnEFTPMC <zackhhi@gmail.com>
2024-09-18 14:39:47 -0400

    redirect

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -37,6 +37,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         const { error } = await supabase.auth.signInWithOAuth({
             provider: "google",
             options: {
+                redirectTo: window.location.origin,
                 queryParams: {
                     access_type: "offline",
                     prompt: "consent",
